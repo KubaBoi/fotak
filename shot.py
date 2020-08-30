8ee99db32df2973fec8576b90e9a53c809ca6b91
KubaBoi <jakubanderle@outlook.cz>
2020-08-30 23:10:30 +0200

    com1

--- shot.py
+++ shot.py
@@ -1,7 +1,10 @@
+#sudo nano /etc/rc.local --- python /var/www/html/fotak/shot.py
 from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
+
+sleep(20)
 
 camera.rotation = 270
 camera.start_preview()
