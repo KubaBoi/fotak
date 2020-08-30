ad24021434a37145d2923cc844fbfd9e958c8429
KubaBoi <jakubanderle@outlook.cz>
2020-08-30 22:54:59 +0200

    rotace - 270 - done

--- shot.py
+++ shot.py
@@ -3,7 +3,7 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.rotation = 90
+camera.rotation = 270
 camera.start_preview()
-sleep(5)
-#camera.stop_preview()
+sleep(60)
+camera.stop_preview()
